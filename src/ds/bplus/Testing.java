bc409e0077b757eb81369a64f23729f55af7e4ac
SafaMgdy <safamagdy1999@gmail.com>
2020-04-07 14:31:48 +0200

    7april

--- src/ds/bplus/Testing.java
+++ src/ds/bplus/Testing.java
@@ -7,43 +7,43 @@ import eminem.DBAppException;
 public class Testing {
 	public static void main(String[] args) throws DBAppException {
 		BTree btree = new BTree();
-		btree.insert(1, 1);
+		btree.insert(1, "page1");
 //		btree.insert(1, 1);
 //		btree.insert(1, 1);
 //
-		btree.insert(3, 1);
-		btree.insert(3, 2);
-		btree.insert(3, 1);
-		btree.insert(3, 3);
 //		btree.insert(3, 1);
+//		btree.insert(3, 2);
 //		btree.insert(3, 1);
 //		btree.insert(3, 3);
 //		btree.insert(3, 1);
 //		btree.insert(3, 1);
 //		btree.insert(3, 3);
+//		btree.insert(3, 1);
+//		btree.insert(3, 1);
+//		btree.insert(3, 3);
 
-		btree.insert(4, 1);
-		btree.insert(4, 1);
-		btree.insert(4, 4);
+//		btree.insert(4, 1);
+//		btree.insert(4, 1);
+//		btree.insert(4, 4);
 
-		btree.insert(4, 3);
-		btree.insert(4, 1);
-		btree.insert(4, 1);
-		btree.insert(4, 4);
+//		btree.insert(4, 3);
+//		btree.insert(4, 1);
+//		btree.insert(4, 1);
+//		btree.insert(4, 4);
 
-		btree.insert(4, 3);
-		btree.insert(5, 3);
-		btree.insert(6, 4);
-		btree.insert(7, 3);
-		btree.insert(8, 3);
-		btree.insert(9, 3);
-		btree.insert(10, 3);
-		btree.insert(11, 5);
-		btree.insert(12, 6);
-		btree.insert(13, 2);
-		btree.insert(14, 7);
-		btree.insert(15, 3);
-		btree.insert(16, 3);
+//		btree.insert(4, 3);
+//		btree.insert(5, 3);
+//		btree.insert(6, 4);
+//		btree.insert(7, 3);
+//		btree.insert(8, 3);
+//		btree.insert(9, 3);
+//		btree.insert(10, 3);
+//		btree.insert(11, 5);
+//		btree.insert(12, 6);
+//		btree.insert(13, 2);
+//		btree.insert(14, 7);
+//		btree.insert(15, 3);
+//		btree.insert(16, 3);
 	System.out.println(btree.toString());
 
 		// System.out.println(btree.toString());
@@ -52,7 +52,7 @@ public class Testing {
 
 		// System.out.println(ref.getReferences().size());
 		// btree.update(3, 1, 2);
-//		btree.delete(3, 3);
+//		btree.delete(1, "page1");
 //		btree.delete(3, 3);
 //		btree.delete(3, 3);
 
@@ -66,7 +66,7 @@ public class Testing {
 //		}
 
 //		ArrayList<String> ref = new ArrayList<String>();
-//		ref = btree.rangeMaxSearch(5);
+//		ref = btree.rangeMaxSearch(1);
 //		for (int i = 0; i < ref.size(); i++) {
 //			System.out.println(ref.get(i));
 //		}
